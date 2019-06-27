ee95ca8642b105571377f03f546820f06e86f2b4
Mohsen Seifi <mohsens2@outlook.com>
2019-06-27 16:07:59 +0430

    init

---
+++ NewsDump/NewsDump/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewsDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
