391e874bd66fd36131738e402cb2cb015e65096e
RezaT4795 <reza.talebi.73@outlook.com>
2019-07-01 19:48:59 +0430

    Created Dumpers for all sites

---
+++ NewsDump/NewsDump.Lib/Operations/Sites/FarsnewsDumper.cs
@@ -0,0 +1,35 @@
+using NewsDump.Lib.Model;
+using NewsDump.Lib.Operations.Sites.Interface;
+using NewsDump.Lib.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewsDump.Lib.Operations.Sites
+{
+    class FarsnewsDumper : DumperBase, IDumper
+    {
+        public News ExtractNews(string html)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public void RunAndSave()
+        {
+            var xml = Get(Constants.FarsnewsRss);
+            var feed = GetFeed(xml);
+
+            foreach (var item in feed.Items)
+            {
+                var html = Get(item.Links.FirstOrDefault().Uri.ToString());
+
+                var news = ExtractNews(html);
+
+            }
+
+        }
+    }
+}
