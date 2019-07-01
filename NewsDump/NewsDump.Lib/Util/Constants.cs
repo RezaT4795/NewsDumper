6260d2cffc485a434f7ff1660cdfa5e8867326a3
RezaT4795 <reza.talebi.73@outlook.com>
2019-07-01 19:42:21 +0430

    Added All RSS URLs

--- NewsDump/NewsDump.Lib/Util/Constants.cs
+++ NewsDump/NewsDump.Lib/Util/Constants.cs
@@ -7,5 +7,13 @@ namespace NewsDump.Lib.Util
     static class Constants
     {
         public static string IribRss { get => "http://iribnews.ir/fa/rss/16"; }
+        public static string FarsnewsRss { get => "https://www.farsnews.com/rss/bushehr";}
+        public static string IrnaRss { get => "https://www.irna.ir/rss/tp/1014";}
+        public static string MehrnewsRss { get => "https://www.mehrnews.com/rss/tp/90";}
+        public static string PgnewsRss { get => "http://pgnews.ir/?template=rss";} //May not work properly due to broken RSS
+        public static string SookRss { get => "http://sook.ir/fa/rss/allnews";}
+        public static string TasnimnewsRss { get => "https://www.tasnimnews.com/fa/rss/feed/0/0/53";}
+        public static string YjcRss { get => "https://www.yjc.ir/fa/rss/23";}
+
     }
 }
