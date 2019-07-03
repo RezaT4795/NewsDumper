5875a1879408264a29b57691c056ba1b2aaaf1b9
Mohsen Seifi <mohsens2@outlook.com>
2019-07-03 21:34:10 +0430

    Working on thread lock

--- NewsDump/NewsDump.UI/MainWindow.xaml.cs
+++ NewsDump/NewsDump.UI/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using NewsDump.Lib.Operations;
+using NewsDump.Lib.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +26,38 @@ namespace NewsDump.UI
         public MainWindow()
         {
             InitializeComponent();
+            //EventBus.OnMessageFired += EventBus_OnMessageFired;
+        }
+
+        private void EventBus_OnMessageFired(MessageArgs message)
+        {
+            console.Text += Environment.NewLine;
+            console.Text += message;
+            scroll.ScrollToEnd();
+        }
+
+        private void Clrconsole_Click(object sender, RoutedEventArgs e)
+        {
+            console.Text = string.Empty;
+        }
+
+        private async void Rundumper_Click(object sender, RoutedEventArgs e)
+        {
+            await this.Dispatcher.InvokeAsync(() =>
+            {
+                Thread.Sleep(3000);
+
+            });
+        }
+
+        private void Exportall_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.Sleep(3000);
+        }
+
+        private void Exportthis_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
